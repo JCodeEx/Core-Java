42c36587eb7d703a64d7e8b46e299cb27e57fab0
rasmin <rasminranjangiri8@gmail.com>
2026-01-05 11:50:24 +0530

    Finding the odd even number using GroupingBy method.

--- src/Core/Java/com/java8/FindingOddEvnNumInSingleShort.java
+++ src/Core/Java/com/java8/FindingOddEvnNumInSingleShort.java
@@ -14,19 +14,32 @@ public class FindingOddEvnNumInSingleShort {
 * partitioningBy splits data into two groups based on a boolean condition
 true → even , false → odd
 * */
-        Map<Boolean, List<Integer>> filterList = numberList.stream()
+        Map<Boolean, List<Integer>> filterMap= numberList.stream()
                 .collect(Collectors.partitioningBy(n -> n % 2 == 0));
 
-        List<Integer> evenNumbers=filterList.get(true);
-        List<Integer>oddNumbers=filterList.get(false);
+        List<Integer> evenNumbers=filterMap.get(true);
+        List<Integer>oddNumbers=filterMap.get(false);
 
-        System.out.println("Even numbers: " + evenNumbers);
-        System.out.println("Odd numbers: " + oddNumbers);
+        System.out.println("From Partitioning Even numbers: " + evenNumbers);
+        System.out.println("From Partitioning Odd numbers: " + oddNumbers);
 
         /*
         * By Using GroupBy function
         * */
 
+Map<Boolean,List<Integer>> filterMap2=numberList.stream()
+        .collect(Collectors.groupingBy(n -> n%2==0));
 
+        List<Integer>l1=filterMap2.get(true);
+        List<Integer>l2=filterMap2.get(false);
+        System.out.println("From GroupBy Even numbers:"+l1);
+        System.out.println("From GroupBy Odd numbers:"+l2);
+
+        Map<String, List<Integer>> map =
+                numberList.stream()
+                        .collect(Collectors.groupingBy(n -> n % 2 == 0 ? "Even" : "Odd"));
+
+        System.out.println("From GroupBy and ternary operator  Even numbers: " + map.get("Even"));
+        System.out.println("From GroupBy and ternary operator Odd numbers: " + map.get("Odd"));
     }
 }
